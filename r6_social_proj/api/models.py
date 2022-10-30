d138c7293e041a15bf70a31ea28c2f3088667320
rxfa <rafa3rnrn@gmail.com>
2022-10-30 14:34:52 +0000

    Add Profile model

--- r6_social_proj/api/models.py
+++ r6_social_proj/api/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  balance = models.DecimalField(..., max_digits=12, decimal_places=2)
+  profile_pic = models.ImageField(blank=True, null=True, upload_to='assets/profiles/')
+  bio = models.TextField(blank=True, null=True)
