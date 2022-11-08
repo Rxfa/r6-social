ebbd4cec19adb423846c3a48335c612e3c3add9a
rxfa <rafa3rnrn@gmail.com>
2022-11-08 21:09:02 +0000

    write function to scrape data on staff

--- r6_social_proj/api/scraper.py
+++ r6_social_proj/api/scraper.py
@@ -9,36 +9,64 @@ URL = "https://liquipedia.net/rainbowsix/Portal:Players/All"
 page = requests.get(URL).text
 soup = BeautifulSoup(page, "lxml")
 
-tab = soup.find("table", class_="wikitable")
-
 list_of_players = []
+list_of_staff = []
 
-for element in soup.find_all("table", class_="wikitable"):
-    if element.find("abbr"):
-        continue
-    else:
-        for row in element.find_all("tr"):
-            if row.find("th"):
-                continue
-            else:
-                id = row.find_all("a")[1].text
+def main():
+    for table in soup.find_all("table", class_="wikitable"):
+        # Every table of staff & talents has an 'abbr' tag
+        if table.find("abbr"):
+            get_staff(table)
+        else:
+            get_players(table)
 
-                if row.find("span", class_="team-template-text"):
-                    team = row.find("span", class_="team-template-text").find("a").text
-                else:
-                    team = "LFT"
+def get_staff(table):
+    for row in table.find_all("tr"):
+        if row.find("th"):
+            continue
+        else:
+            country = row.find("img")["alt"]
+            id = row.find_all("a")[1].text
+            real_name = row.find_all("td")[1].text if row.find_all("td")[1] else ""
+            role = row.find_all("td")[2].text if row.find_all("td")[2] else ""
+            team = (
+                row.find("span", class_="team-template-text").find("a").text
+                if row.find("span", class_="team-template-text")
+                else ""
+            )
+            staff_info = {
+                "country": country.strip(),
+                "nickname": id.strip(),
+                "real name": real_name.strip(),
+                "team": team.strip(),
+                "role": role.strip(),
+            }
+            list_of_staff.append(staff_info)
+    return list_of_staff
 
-                if row.find_all("td")[1]:
-                    real_name = row.find_all("td")[1].text
-                else:
-                    real_name = ""
+def get_players(table):
+    for row in table.find_all("tr"):
+        if row.find("th"):
+            continue
+        else:
+            country = row.find("img")["alt"]
+            id = row.find_all("a")[1].text
+            team = (
+                row.find("span", class_="team-template-text").find("a").text
+                if row.find("span", class_="team-template-text")
+                else "LFT"
+            )
+            real_name = row.find_all("td")[1].text if row.find_all("td")[1] else ""
+            player_info = {
+                "country": country,
+                "nickname": id.strip(),
+                "real name": real_name.strip(),
+                "team": team.strip(),
+                "role": "Player",
+            }
+            list_of_players.append(player_info)
+    return list_of_players
 
-                player_info = {
-                    'nickname': id,
-                    'real name': real_name.strip(),
-                    'team': team if team else 'LFT',
-                }
-                list_of_players.append(player_info)
 
-#print(list_of_players)
-print(len(list_of_players))
+if __name__ == "__main__":
+    main()
