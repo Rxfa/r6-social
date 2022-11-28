d372da332ac1d5c738ca1e530a0362b6cedf0fc8
rxfa <rafa3rnrn@gmail.com>
2022-11-28 22:18:07 +0000

    Fixed upcoming games bug

--- r6_social_proj/api/scraping/R6_GAMES.py
+++ r6_social_proj/api/scraping/R6_GAMES.py
@@ -18,32 +18,24 @@ def main():
 
 def get_game_data(url):
     logger.info("Scraping future games data")
-    page = requests.get(url,timeout=10)
-    soup = BeautifulSoup(page.content,"html.parser")
-    for table in soup.find_all("div",class_="active"):
-            get_games(table)
+    page = requests.get(url, timeout=10)
+    soup = BeautifulSoup(page.content, "html.parser")
+    upcoming_games = soup.select("div[id^='tab-upcoming'] a")
+    game_list = [get_game(game) for game in upcoming_games]
     logger.info("Game data scraped")
-    export_to_json("game_data",games_list)
+    export_to_json("game_data", [get_game(game) for game in upcoming_games])
 
 
-def get_games(table):
+def get_game(game):
+    """Returns upcoming game information"""
     logger.info("Scraping games")
-    for row in table.find_all("a",class_="match"):  #Bug 
-             country = row.find("img",class_="flag")["alt"]
-             hour = row.find("span",class_="meta__item meta__time").text
-             date = row.find("span",class_="meta__item meta__day").text
-             team = row.find("span", class_="match__name text-truncate").text
-             tournmanet = row.find("span",class_="meta__item meta__competition").text  
-    game_info ={
-            "team": team,
-            "country":country,
-            "date":date,
-            "hour": hour,
-            "tournament":tournmanet
-            }
-    games_list.append(game_info)
-    logger.info("Scraped successfully")
-    return games_list
+    return dict(
+        date=game.select_one("span[class='meta__item meta__day']").text,
+        time=game.select_one("span[class='meta__item meta__time']").text,
+        competition=game.select_one("span[class='meta__item meta__competition']").text,
+        team1=game.select_one("div[class='match__table match__table--b match__table--']:nth-of-type(2) div span[class^='match__name']").text,
+        team2=game.select_one("div[class='match__table match__table--b match__table--']:nth-of-type(4) div span[class^='match__name']").text
+    )
 
 
 def export_to_json(filename, data):
@@ -51,7 +43,7 @@ def export_to_json(filename, data):
     Exports given data list to JSON file.
     """
     logger.info("Exporting %s to JSON file.", filename)
-    with open(f"json/{filename}.json", "w+", encoding="utf-8") as file:
+    with open(f"../json/{filename}.json", "w+", encoding="utf-8") as file:
         file.write(json.dumps(data, ensure_ascii=False, indent=4))
         logger.info("%s exported to json/%s.json successfully!", data, filename)
 
